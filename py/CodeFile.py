671eed001ee14f2201e2d87a1ba4c687eac366b9
Scott8440 <Scott8440@gmail.com>
2018-02-27 17:30:16 -0500

    remove unused code

--- py/CodeFile.py
+++ py/CodeFile.py
@@ -18,23 +18,3 @@ class CodeFile:
 
     def addClass(self, block):
         self.classes.append(block)
-
-    def parseFile(self, filename):
-       codeParser = CodeParser()
-       with open(filename) as f:
-            lines = f.readlines()
-            lineNum = 0
-            while lineNum < len(lines):
-                line = lines[lineNum]
-                if 'class' in line.split(' '):
-                    print("****Parsing CodeClass: {}".format(line.rstrip()))
-                    classLines, lineNum = codeParser.packageBlock(lines, lineNum)
-                    self.classes.append(codeParser.parseClass(classLines))
-                elif 'def' in line.split(' '):
-                    print("****Parsing CodeFunction: {}".format(line.rstrip()))
-                    functionLines, lineNum = codeParser.packageBlock(lines, lineNum)
-                    self.functions.append(codeParser.parseFunction(functionLines))
-                else:
-                    #print("Random Line: '{}'".format(line.rstrip()))
-                    lineNum += 1
-                line = f.readline()
