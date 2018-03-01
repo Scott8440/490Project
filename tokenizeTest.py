9ed5e16672b357c5004d6f8d01d19fc066869be8
Scott8440 <Scott8440@gmail.com>
2018-03-01 14:30:12 -0500

    directory cleanup

--- tokenizeTest.py
+++
@@ -1,6 +0,0 @@
-import tokenize
-
-with open('example.py', 'r') as s:
-    g = tokenize.generate_tokens(s.readline)
-    for toknum, tokval, a, b, c in g:
-        print("{}, {}, {}, {}".format(toknum, tokval, a, b))
