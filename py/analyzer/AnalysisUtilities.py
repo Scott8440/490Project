4e58932121948c8474ff670fe74d828cbdacc0c9
Scott8440 <Scott8440@gmail.com>
2018-03-07 11:34:24 -0500

    capture floating point numbers with regex for magic numbers

--- py/analyzer/AnalysisUtilities.py
+++ py/analyzer/AnalysisUtilities.py
@@ -5,7 +5,8 @@ import re
 def extractMagicNumbers(text):
     strippedText = LineHelpers.stripLine(text)
     # REGEX matches all numbers not following an equals sign or within a word
-    numberList = re.findall(r"(?<!= )(?<!=)(?<!\w)\d+(?!\w)", strippedText)
+    
+    numberList = re.findall(r"(?<!\w)[-+]?\d*\.?\d+(?!\w)", strippedText)
     for i in range(len(numberList)):
         numberList[i] = int(numberList[i])
     return numberList
