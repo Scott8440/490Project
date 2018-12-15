0cb36dc35dcccf548f0f87af922bdf89d3b676a7
Scott8440 <smithscott@fb.com>
2018-12-15 10:01:30 -0800

    lint fix parser_test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,15 +1,9 @@
 import unittest
 from py.parser.CodeFactory import CodeFactory
-from py.code_rep.CodeClass import CodeClass
-from py.code_rep.CodeFunction import CodeFunction
-from py.code_rep.CodeBlock import CodeBlock
-from py.code_rep.CodeLine import CodeLine
 from py.code_rep.LineTypes import LineTypes
-import py.code_rep.LineHelpers as LineHelpers
 
 
 class TestParser(unittest.TestCase):
-
 
     def testExampleFile(self):
         path = 'example.py'
@@ -24,13 +18,8 @@ class TestParser(unittest.TestCase):
         self.assertEqual(functions[0].name, func1Name)
         self.assertEqual(functions[0].arguments, func1Args)
 
-        # Check Classes
         self.assertEqual(len(codeFile.classes), 1)
-
-        # Check Blocks
         self.assertEqual(len(codeFile.blocks), 1)
-
-        # Check Lines
         self.assertEqual(len(codeFile.lines), 6)
 
     def testMultilineFile(self):
@@ -67,14 +56,13 @@ class TestParser(unittest.TestCase):
         parser = CodeFactory.createParser(path)
         parser.logicalLines = parser.removeEscapeNewlines()
         codedLines = parser.codifyLines()
-        self.assertEqual(codedLines[0].lineNumber,1) # check that line numbers are not 0-indexed
+        self.assertEqual(codedLines[0].lineNumber, 1) # check that line numbers are not 0-indexed
         self.assertEqual(codedLines[0].indentation, 0)
         self.assertEqual(codedLines[1].indentation, 4)
         self.assertEqual(codedLines[2].indentation, 4) # indentation remains same throughout multiline segment
         self.assertEqual(codedLines[3].indentation, 0)
         self.assertEqual(codedLines[4].indentation, 4)
         self.assertEqual(codedLines[5].indentation, 4)
-
 
         self.assertEqual(codedLines[6].indentation, 0)
         self.assertEqual(codedLines[7].indentation, 4)
@@ -89,6 +77,7 @@ class TestParser(unittest.TestCase):
         self.assertEqual(codedLines[19].lineType, LineTypes.CONTINUES_MULTILINE_STRING)
 
         self.assertEqual(codedLines[20].lineType, LineTypes.ENDS_DOUBLE_MULTILINE_STRING)
+
 
 if __name__ == '__main__':
     unittest.main()
